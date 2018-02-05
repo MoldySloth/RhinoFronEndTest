88c78f3ab14b2c712e86468c938505aa1223e88c
MoldySloth <rozanne.dejager@gmail.com>
2018-02-05 15:38:09 +0200

    Start of project
    Start of Rhino front end project

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Front-end Test</title>
+    </head>
+    <body>
+        <img src="#"><a href="https://www.rhinoafrica.com/en?_ptp=1"></a>
+        <h1>Just some nice images</h1>
+        <p>This is a project for rhinoafrica.com<br>It should verify my skills a front end developer</p>
+    </body>
+</html>
