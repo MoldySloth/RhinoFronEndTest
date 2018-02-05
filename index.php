9ac9cde5e543587248fcc1e172b3d4e19d91fc85
MoldySloth <rozanne.dejager@gmail.com>
2018-02-05 16:38:14 +0200

    HTML
    Working on html

--- index.php
+++ index.php
@@ -2,10 +2,22 @@
 <html>
     <head>
         <title>Front-end Test</title>
+<!--        link css-->
     </head>
     <body>
+    <div>
+<!--        current content on page-->
         <img src="#"><a href="https://www.rhinoafrica.com/en?_ptp=1"></a>
         <h1>Just some nice images</h1>
         <p>This is a project for rhinoafrica.com<br>It should verify my skills a front end developer</p>
+    </div>
+    <div>
+<!--        where photo's and stuff go-->
+        <div>
+<!--            photo div-->
+        </div>
+    </div>
+<!--    link js-->
+    <script></script>
     </body>
 </html>
