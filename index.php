80aaea2d92b7e5659106de8e064e909008bae79a
MoldySloth <rozanne.dejager@gmail.com>
2018-02-06 18:02:44 +0200

    modal and image functional

--- index.php
+++ index.php
@@ -1,23 +1,45 @@
 <!DOCTYPE html>
-<html>
+<html lang="en-us">
     <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="IE=11; IE=Edge; chrome=1">
         <title>Front-end Test</title>
-<!--        link css-->
+        <!--        link to font-->
+        <link href="https://fonts.googleapis.com/css?family=Open Sans" rel="stylesheet">
+        <!--        link css-->
+        <link rel="stylesheet" href="css/style.css">
     </head>
     <body>
-    <div>
-<!--        current content on page-->
-        <img src="#"><a href="https://www.rhinoafrica.com/en?_ptp=1"></a>
+    <div class="container">
+        <!--        current content on page-->
+        <a href="https://www.rhinoafrica.com/en?_ptp=1"><img class="logo" src="resources/RhinoAfricacom_RGB.png"></a>
         <h1>Just some nice images</h1>
         <p>This is a project for rhinoafrica.com<br>It should verify my skills a front end developer</p>
     </div>
-    <div>
-<!--        where photo's and stuff go-->
-        <div>
-<!--            photo div-->
+    <!--        where photo's and stuff go-->
+    <div class="gallery">
+        <!--            photo div-->
+        <div class="myPhoto">
+            <img class="photo" id="photoRound" src="resources/pexels-photo-416202.jpeg" alt="" onclick="modal(this);">
+        </div>
+        <div class="myPhoto">
+            <img class="photo" id="photoRectangle" src="resources/pexels-photo-631292.jpeg" alt="" onclick="modal(this);">
+        </div>
+        <div class="myPhoto">
+            <img class="photo" id="photoSquare" src="resources/pexels-photo-187842.jpeg" alt="" onclick="modal(this);">
+        </div>
+        <div class="myPhoto">
+            <img class="photo" id="photoTriangle" src="resources/pexels-photo-247376.jpeg" alt="" onclick="modal(this);">
         </div>
     </div>
-<!--    link js-->
-    <script></script>
+    <!--    Modal box-->
+    <div id="myModal" class="modal">
+        <span class="close">&times;</span>
+        <img class="modal-content" id="imgModal">
+        <div id="caption"></div>
+    </div>
+    <!--    link js-->
+    <script src="js/photo.js"></script>
     </body>
 </html>
---
+++ js/photo.js
@@ -0,0 +1,23 @@
+function modal(element) {
+    //Get the modal
+    var image = document.getElementById(element.id);
+    var myModal = document.getElementById('myModal');
+
+    //insert the image into the modal
+    var modalImg = document.getElementById('imgModal');
+    var captionText = document.getElementById('caption');
+    myModal.style.display = "block";
+    modalImg.src = element.src;
+    captionText.innerHTML = element.alt;
+}
+
+//Get the <span> element that closes the modal
+var span = document.getElementsByClassName('close')[0];
+var myModal = document.getElementById('myModal');
+
+//When the (x) is clicked the modal should close
+span.onclick = function () {
+    myModal.style.display = "none";
+};
+
+
